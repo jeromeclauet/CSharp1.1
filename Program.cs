59a06cce05bd9550293f0c9a2e0a91cc704765ad
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 10:55:17 +0100

    Exercice 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 7;
+            int secondNumber = 11;
+            int thirdNumber = 12;
+
+            int sum = firstNumber + secondNumber;
+            int result = sum * thirdNumber;
+
+            Console.WriteLine("Le résultat du premier calcul est " + sum );
+            Console.WriteLine("Le résultat du second calcul est " + result);
+
+
+        }
+    }
+}
